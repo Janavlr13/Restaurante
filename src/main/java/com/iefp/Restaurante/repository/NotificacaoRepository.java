ee00fb7c4d2effd3812636eb5aeb5022fda2f795
Janavlr13 <janavlr13@gmail.com>
2026-05-05 19:33:07 +0100

    Erros no NotificacaoController.java corrigidos. Em NotificacaoRepository.java estava <Mesa, Long> em vez de <Notificacao, Long>

--- src/main/java/com/iefp/Restaurante/repository/NotificacaoRepository.java
+++ src/main/java/com/iefp/Restaurante/repository/NotificacaoRepository.java
@@ -1,9 +1,8 @@
 package com.iefp.Restaurante.repository;
 
 import com.iefp.Restaurante.model.Mesa;
+import com.iefp.Restaurante.model.Notificacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificacaoRepository extends JpaRepository<Mesa, Long> {
-
-
+public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {
 }
