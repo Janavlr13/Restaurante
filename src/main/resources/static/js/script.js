1f3db679f3d30fa3c316656b957475cb3edc0180
Janavlr13 <janavlr13@gmail.com>
2026-05-07 16:37:30 +0100

    Criação das funções marcar.... para todas as entidades em script.js e modificação dos ficheiros .html para mostrar as mesmas.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,6 +10,40 @@ function marcarCliente(){
         nomes[i].classList.toggle('destacado');
     }
 }
+function marcarGerente() {
+    const nomes = document.querySelectorAll('.coluna-nome');
+    for(let i = 0; i < nomes.length; i++){
+        nomes[i].classList.toggle('destacado');
+    }
+}
+
+function marcarFuncionario() {
+    const nomes = document.querySelectorAll('.coluna-numeroMesa');
+    for(let i = 0; i < nomes.length; i++){
+        nomes[i].classList.toggle('destacado');
+    }
+}
+
+function marcarMesa() {
+    const nomes = document.querySelectorAll('.coluna-numeroMesa');
+    for(let i = 0; i < nomes.length; i++){
+        nomes[i].classList.toggle('destacado');
+    }
+}
+
+function marcarReserva() {
+    const nomes = document.querySelectorAll('.coluna-nome');
+    for(let i = 0; i < nomes.length; i++){
+        nomes[i].classList.toggle('destacado');
+    }
+}
+
+function marcarNotificacao() {
+    const nomes = document.querySelectorAll('.coluna-dataHora');
+    for(let i = 0; i < nomes.length; i++){
+        nomes[i].classList.toggle('destacado');
+    }
+}
 
 function toggleTabela(){
     let tabela = document.getElementById("tabelaClientes");
